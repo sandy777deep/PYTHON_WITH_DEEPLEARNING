451aa4f25684256ae4e2efb8b09c19bdb7dd0ce8
sandy777deep <66700051+sandy777deep@users.noreply.github.com>
2020-06-10 21:18:36 -0500

    ICP1 updated

---
+++ Python_ICP1/ArthematicOperations.py
@@ -0,0 +1,23 @@
+
+# Taking integer input from the user
+Integer1 = input('Please input value of integer A')
+Integer2 = input('Please input value of integer B')
+
+# converting them into integer
+A=int(Integer1)
+B=int(Integer2)
+
+print('Here are some arithmetic  operations performed on integers ')
+
+# Addition
+# storing the sum in integer C
+c = A+B
+print('Addition operation A+B=',c)
+
+# Subtraction
+d = A-B
+print('subtraction operation A-B',d)
+
+# Multiplication
+e = A*B
+print('Multiplication operation A*B',e)
