0737c555c98011522e04af8a7976ed70a08c4d7f
sandy777deep <66700051+sandy777deep@users.noreply.github.com>
2020-07-14 23:06:35 -0500

    Add files via upload

---
+++ DeepLearning_Lesson3/Bonus1.py
@@ -0,0 +1,43 @@
+from keras.models import Sequential
+from keras import layers
+from keras.preprocessing.text import Tokenizer
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from keras.layers import Embedding
+import matplotlib.pyplot as plt
+from keras.layers import Flatten
+from keras.callbacks import TensorBoard
+from time import time
+df = pd.read_csv('imdb_master.csv',encoding='latin-1')
+print(df.head())
+sentences = df['review'].values
+y = df['label'].values
+#tokenizing data
+tokenizer = Tokenizer(num_words=2000)
+tokenizer.fit_on_texts(sentences)
+#getting the vocabulary of data
+sentences = tokenizer.texts_to_matrix(sentences)
+le = preprocessing.LabelEncoder()
+y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(sentences, y, test_size=0.25, random_state=1000)
+#print(input_dim)
+model = Sequential()
+model.add(Embedding(2000, 50, input_length=2000))
+model.add(Flatten())
+model.add(layers.Dense(300,input_dim=2000, activation='relu'))
+model.add(layers.Dense(3,activation='softmax'))
+tensorborad = TensorBoard(log_dir="logs/{}".format(time()))
+model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['acc'])
+model.fit(X_train, y_train,
+          batch_size=256,
+          epochs=2,
+          verbose=1,
+          validation_data=(X_test, y_test),
+          callbacks=[tensorborad])
+history=model.fit(X_train,y_train, epochs=3, verbose=True, validation_data=(X_test,y_test), batch_size=256)
+# For accuracy values
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
